48620a5b0a44b9d8d22e8b614bb5048cd79d6a80
HARADA Makoto <harada@toydea.com>
2020-04-26 11:57:28 +0900

    Add struct Serial

--- src/main.c
+++ src/main.c
@@ -2,20 +2,22 @@
 #include "asm.h"
 #include "serial.h"
 
+static Serial com1_;
+
 void KernelEntry(LiumOS* liumos_passed)
 {
-	serial_init(0x2f8);
+	serial_init(&com1_, 0x2f8);
 
 	char buf[256];
 	snprintf(buf, sizeof(buf), "hoge %d\nFUGA", 1);
 
 	for(int i=0; buf[i] != '\0'; i++){
-		serial_send_char(buf[i]);
+		serial_send_char(&com1_, buf[i]);
 	}
 
 	while(1);
 		
 	while(1){
-		serial_send_char('a');
+		serial_send_char(&com1_, 'a');
 	}
 }
